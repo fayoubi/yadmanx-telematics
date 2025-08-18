563018bb7961de8a50d269c81db3e199a025d2b5
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2025-08-18 15:53:06 +0300

    Migrate SDK to Scene Delegate

--- DemoSwiftApp/AppDelegate.swift
+++ DemoSwiftApp/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RPSpeedLimitDelegate, RPT
     }
 
     func trackingStateChanged(_ state: Bool) {
-        var body = "Tracking Stoped"
+        var body = "Tracking Stopped"
         if state {
             body = "Tracking Started"
         }
@@ -84,66 +84,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RPSpeedLimitDelegate, RPT
         
         RPEntry.initializeSDK()
         
-        let appName = "\"Swift Demo App\""
-        let pages = [
-            PageHelper.createPage(
-                pageHeader   : "Attention Required!",
-                pageText     : "\n\nYou will receive a popup shortly. This popup will ask you for access to your location even when you are not using the app.\n\nPlease, choose ",
-                pageBoldText : "“Allow while using App”",
-                lastPageText : " for \(appName) to automatically detect when you start a trip.",
-                alertHeader  : "Allow \(appName) to access your location?",
-                alertText    : "\n\nWe use location service for evaluating your driving manner. By enabling “Always While Using App” on location, the app will be able to automatically determine when you have started and ended a drive",
-                buttonTexts  : ["Allow while using App", "Allow Once", "Don’t Allow"],
-                coloredButton: "Allow while using App"),
-            PageHelper.createPage(
-                pageHeader   : "Attention Required!",
-                pageText     : "\n\nThen, You will receive a popup to grant us access to your Motion & Fitness activity. We need this information to create a driving score.\n\nPlease, choose ",
-                pageBoldText : "“OK”",
-                lastPageText : " for \(appName) to grant us access to Motion & Fitness activity.",
-                alertHeader  : "\(appName) Would Like to Access Your Motion & Fitness Activity",
-                alertText    : "\n\nWe use motion sensors for evaluating your driving manner",
-                buttonTexts  : ["Don’t Allow", "OK"],
-                coloredButton: "OK"),
-            PageHelper.createPage(
-                    pageHeader   : "Attention Required!",
-                    pageText     : "\n\nAs the next step, You will receive a popup to grant us rights to send you important notifications. You are always able to turn off notifications by categories in the App settings menu.\n\nPlease, choose ",
-                    pageBoldText : "“Allow”",
-                    lastPageText : " for \(appName) to keep you informed on important events.",
-                    alertHeader  : "\(appName) Would Like to Send You Notifications",
-                    alertText    : "\n\nNotifications may include alerts, sounds and icon badges. These can be configured in Settings",
-                    buttonTexts  : ["Don’t Allow", "Allow"],
-                    coloredButton: "Allow"),
-            PageHelper.createPage(
-                pageHeader   : "Attention Required!",
-                pageText     : "\n\nYou will receive a popup shortly for \(appName) to have access to your location even when you are not using the app.\n\nFor the app to work properly\n",
-                pageBoldText : "“Change to Always Allow”",
-                lastPageText : "",
-                alertHeader  : "Allow \(appName) to also access your location even when you are not using the app?",
-                alertText    : "\n\nWe use location service for generating a driving score. By enabling “Always” on location, the app will be able to automatically determine when you have started and ended a drive",
-                buttonTexts  : ["Keep Only While Using", "Change to Always Allow"],
-                coloredButton: "Change to Always Allow")
-        ]
-
-        RPSettings.returnInstance().wizardPages = pages
-        
-        //Customization
-        RPSettings.returnInstance().wizardNextButtonBgColor = UIColor.red
-        RPSettings.returnInstance().wizardMaintextColor = UIColor.black
-        RPSettings.returnInstance().wizardAlertBackgroundColor = UIColor.white
-        RPSettings.returnInstance().wizardAlertTextColor = UIColor.green
-        RPSettings.returnInstance().wizardAlertButtonTextColor = UIColor.blue
-        RPSettings.returnInstance().wizardAlertActiveButtonTextColor = UIColor.red
-        RPSettings.returnInstance().wizardAlertActiveButtonBgColor = UIColor.gray
-        RPSettings.returnInstance().appName = "TelematicsApp"
-        
-        //Dark Scheme
-        //RPSettings.returnInstance().wizardAlertBackgroundColor = UIColor.white
-        //RPSettings.returnInstance().wizardAlertTextColor = UIColor.white
-        //RPSettings.returnInstance().wizardAlertButtonTextColor = UIColor.white
-        //RPSettings.returnInstance().wizardAlertActiveButtonTextColor = UIColor.white
-        //RPSettings.returnInstance().wizardAlertActiveButtonBgColor = UIColor.white
-        
-        //FIXME: VIRTUAL DEVICE TOKEN REQUIRED!
+        //VIRTUAL DEVICE TOKEN REQUIRED!
         let token = "VIRTUAL_DEVICE_TOKEN" //REQUIRED!
         RPEntry.instance.virtualDeviceToken = token
         RPEntry.instance.trackingStateDelegate = self
@@ -155,27 +96,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RPSpeedLimitDelegate, RPT
             RPEntry.instance.advertisingIdentifier = ASIdentifierManager.shared().advertisingIdentifier
         }
         
-        RPPermissionsWizard.returnInstance().launch(finish: { finished in
-            print("wizard finished: \(finished)")
-        })
-        
         RPEntry.instance.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
     
     // MARK: SDK Integration
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        RPEntry.instance.applicationDidBecomeActive(application)
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        RPEntry.instance.applicationDidEnterBackground(application)
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        RPEntry.instance.applicationWillEnterForeground(application)
-    }
     
     func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
         RPEntry.instance.applicationDidReceiveMemoryWarning(application)
@@ -187,9 +112,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RPSpeedLimitDelegate, RPT
     
     func application(
         _ application: UIApplication,
-        performFetchWithCompletionHandler completionHandler: @escaping (
-            UIBackgroundFetchResult
-        ) -> Void
+        performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
     ) {
         RPEntry.instance.application(application) {
             completionHandler(.newData)
@@ -203,8 +126,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RPSpeedLimitDelegate, RPT
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -212,9 +133,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RPSpeedLimitDelegate, RPT
         _ application: UIApplication,
         didDiscardSceneSessions sceneSessions: Set<UISceneSession>
     ) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        
     }
 
     func application(
--- DemoSwiftApp/SceneDelegate.swift
+++ DemoSwiftApp/SceneDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Foundation
+import TelematicsSDK
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -14,39 +16,91 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        window?.overrideUserInterfaceStyle = .light
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        window?.overrideUserInterfaceStyle = .light
+        
+        let appName = "\"Swift Demo App\""
+        let pages = [
+            PageHelper.createPage(
+                pageHeader   : "Attention Required!",
+                pageText     : "\n\nYou will receive a popup shortly. This popup will ask you for access to your location even when you are not using the app.\n\nPlease, choose ",
+                pageBoldText : "“Allow while using App”",
+                lastPageText : " for \(appName) to automatically detect when you start a trip.",
+                alertHeader  : "Allow \(appName) to access your location?",
+                alertText    : "\n\nWe use location service for evaluating your driving manner. By enabling “Always While Using App” on location, the app will be able to automatically determine when you have started and ended a drive",
+                buttonTexts  : ["Allow while using App", "Allow Once", "Don’t Allow"],
+                coloredButton: "Allow while using App"),
+            PageHelper.createPage(
+                pageHeader   : "Attention Required!",
+                pageText     : "\n\nThen, You will receive a popup to grant us access to your Motion & Fitness activity. We need this information to create a driving score.\n\nPlease, choose ",
+                pageBoldText : "“OK”",
+                lastPageText : " for \(appName) to grant us access to Motion & Fitness activity.",
+                alertHeader  : "\(appName) Would Like to Access Your Motion & Fitness Activity",
+                alertText    : "\n\nWe use motion sensors for evaluating your driving manner",
+                buttonTexts  : ["Don’t Allow", "OK"],
+                coloredButton: "OK"),
+            PageHelper.createPage(
+                    pageHeader   : "Attention Required!",
+                    pageText     : "\n\nAs the next step, You will receive a popup to grant us rights to send you important notifications. You are always able to turn off notifications by categories in the App settings menu.\n\nPlease, choose ",
+                    pageBoldText : "“Allow”",
+                    lastPageText : " for \(appName) to keep you informed on important events.",
+                    alertHeader  : "\(appName) Would Like to Send You Notifications",
+                    alertText    : "\n\nNotifications may include alerts, sounds and icon badges. These can be configured in Settings",
+                    buttonTexts  : ["Don’t Allow", "Allow"],
+                    coloredButton: "Allow"),
+            PageHelper.createPage(
+                pageHeader   : "Attention Required!",
+                pageText     : "\n\nYou will receive a popup shortly for \(appName) to have access to your location even when you are not using the app.\n\nFor the app to work properly\n",
+                pageBoldText : "“Change to Always Allow”",
+                lastPageText : "",
+                alertHeader  : "Allow \(appName) to also access your location even when you are not using the app?",
+                alertText    : "\n\nWe use location service for generating a driving score. By enabling “Always” on location, the app will be able to automatically determine when you have started and ended a drive",
+                buttonTexts  : ["Keep Only While Using", "Change to Always Allow"],
+                coloredButton: "Change to Always Allow")
+        ]
+        
+        RPSettings.returnInstance().wizardPages = pages
+        
+        //Customization
+        RPSettings.returnInstance().wizardNextButtonBgColor = UIColor.red
+        RPSettings.returnInstance().wizardMaintextColor = UIColor.black
+        RPSettings.returnInstance().wizardAlertBackgroundColor = UIColor.white
+        RPSettings.returnInstance().wizardAlertTextColor = UIColor.green
+        RPSettings.returnInstance().wizardAlertButtonTextColor = UIColor.blue
+        RPSettings.returnInstance().wizardAlertActiveButtonTextColor = UIColor.red
+        RPSettings.returnInstance().wizardAlertActiveButtonBgColor = UIColor.gray
+        RPSettings.returnInstance().appName = "TelematicsApp"
+        
+        //Dark Scheme
+        //RPSettings.returnInstance().wizardAlertBackgroundColor = UIColor.white
+        //RPSettings.returnInstance().wizardAlertTextColor = UIColor.white
+        //RPSettings.returnInstance().wizardAlertButtonTextColor = UIColor.white
+        //RPSettings.returnInstance().wizardAlertActiveButtonTextColor = UIColor.white
+        //RPSettings.returnInstance().wizardAlertActiveButtonBgColor = UIColor.white
+        
+        RPPermissionsWizard.returnInstance().launch(finish: { finished in
+            print("wizard finished: \(finished)")
+        })
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+        
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        RPEntry.instance.sceneDidBecomeActive(scene)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        RPEntry.instance.sceneWillEnterForeground(scene)
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+        RPEntry.instance.sceneDidEnterBackground(scene)
     }
 
 
